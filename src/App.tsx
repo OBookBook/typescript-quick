34b50cc5f489eed054efe43f54093f8a51642414
N.I <iworksWeb@gmail.com>
2025-02-27 01:01:19 +0900

    enum (列挙型)

--- src/App.tsx
+++ src/App.tsx
@@ -89,6 +89,33 @@ type KEYS = {
 let key: keyof KEYS;
 key = "primary";
 
+// typeof + keyof
+const SPORTS = {
+  soccer: "Soccer",
+  baseball: "baseball",
+};
+let keySport: keyof typeof SPORTS;
+keySport = "soccer";
+
+// enum (列挙型)
+enum OS {
+  Windows, // 0
+  Mac, // 1
+  Linux, // 2
+}
+interface PC {
+  id: number;
+  OSType: OS;
+}
+const PC1: PC = {
+  id: 1,
+  OSType: OS.Windows,
+};
+const PC2: PC = {
+  id: 2,
+  OSType: OS.Mac,
+};
+
 function App() {
   return (
     <>
